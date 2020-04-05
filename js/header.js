19e45360d38ad333b960157c259bdf362c9a2159
Evgeny <57188984+eozubarev@users.noreply.github.com>
2020-04-05 16:19:54 +0300

    add new js files
    Form JS
    Mail php

--- js/header.js
+++ js/header.js
@@ -1,15 +1,15 @@
-(function() {
-  if (window.matchMedia("(max-width: 992px)").matches) {
+; (function () {
+  if (window.matchMedia('(max-width: 992px)').matches) {
     return;
   }
 
-  var headerPage = document.querySelector(".header-page");
+  var headerPage = document.querySelector('.header-page');
 
-  window.addEventListener("scroll", function() {
+  window.addEventListener('scroll', function () {
     if (window.pageYOffset > 0) {
-      headerPage.classList.add("is-active");
+      headerPage.classList.add('is-active');
     } else {
-      headerPage.classList.remove("is-active");
+      headerPage.classList.remove('is-active');
     }
   });
 })();
